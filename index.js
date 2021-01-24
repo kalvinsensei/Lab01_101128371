1b66a7b1f83d13f7676b434f69c7fdc6f35a7999
kalvinsensei <kalvin_1999@hotmail.com>
2021-01-23 20:25:54 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+
+var countries = ['./canada.txt', './usa.txt'];
+
+fs.unlink('canada.txt', (err) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+})
+
+fs.unlink('usa.txt', (err) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+})
+
+fs.createReadStream('input_countries.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+     if (row["country"] == "Canada") {
+       fs.appendFile('canada.txt', String(row["country"] + ',' + row["year"] + ',' + row["population"] + '\n'), (err) => {
+           if (err) throw err;
+       });
+     }
+     else if (row["country"] == "United States") {
+       fs.appendFile('usa.txt', String(row["country"] + ',' + row["year"] + ',' + row["population"] + '\n'), (err) => {
+           if (err) throw err;
+       });
+     }
+  });
